08936d9190ef449312007bb271adc5aed52ec39e
RocioMartinez98 <rocioabigailmartinez98@gmail.com>
2024-02-03 00:18:11 -0300

    version 3

--- contactlistweb/src/app/app.routes.ts
+++ contactlistweb/src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-    path:'',
-    loadComponent: () => import('./contact-list/contact-list.component')
+        path:'',
+        loadComponent: () => import('./contact-list/contact-list.component')
+    },
+    {
+        path:'new',
+        loadComponent: () => import('./contact-form/contact-form.component')
     }
 ];
---
+++ contactlistweb/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [RouterModule, ReactiveFormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.css'
+})
+export default class ContactFormComponent {
+
+}
